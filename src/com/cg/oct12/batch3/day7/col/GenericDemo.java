dea1e8141c766a84f343d3c06f31b30ecf382d8f
Aditi16patil <59495375+Aditi16patil@users.noreply.github.com>
2021-10-26 09:50:11 +0530

    Initial commit

---
+++ src/com/cg/oct12/batch3/day7/col/GenericDemo.java
@@ -0,0 +1,30 @@
+package com.cg.oct12.batch3.day7.col;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class GenericDemo {
+
+
+
+		public static void main(String[] args) {
+
+//			ArrayList myMarks = new ArrayList();
+//			myMarks.add(10);
+//			myMarks.add(10.5);
+//			myMarks.add("abc");
+//			System.out.println(myMarks);
+//			Iterator it = myMarks.iterator();
+//			while (it.hasNext())
+//				System.out.println(it.next());
+
+			ArrayList<Integer> myMarks = new ArrayList<>();//we can also write like this new ArrayList<INteger>()
+			myMarks.add(10);
+//			myMarks.add(10.5);
+//			myMarks.add("abc");
+			myMarks.add(25);
+			System.out.println(myMarks);
+
+		}
+
+	}
